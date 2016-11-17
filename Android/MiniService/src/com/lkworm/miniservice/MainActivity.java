d00c992aba2e03acc2d871661735e289e329b3de
erisir <n.daguan@gmail.com>
2016-11-17 18:41:21 +0800

    ui OK

--- Android/MiniService/src/com/lkworm/miniservice/MainActivity.java
+++ Android/MiniService/src/com/lkworm/miniservice/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -21,8 +22,15 @@ public class MainActivity extends Activity {
 			String val = ((Bundle)msg.obj).getString("value") ;
 			switch (msg.what) {
 			case 1111:
-				if(showLogCheck.isChecked())
+				if(showLogCheck.isChecked()){
 					msgText.setText(msgText.getText()+"\r\n"+val);
+					mHandler.post(new Runnable() {  
+					    @Override  
+					    public void run() {  
+					    	scrollView.fullScroll(ScrollView.FOCUS_DOWN);   
+					    }  
+					});
+				}
 				break;
 			case 9999:
 				msgText.setText("\r\n"+val);
@@ -32,12 +40,14 @@ public class MainActivity extends Activity {
 		}
 	};
 	private static CheckBox showLogCheck;
+	private static ScrollView scrollView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		msgText = (TextView)findViewById(R.id.msg);
 		showLogCheck = (CheckBox)findViewById(R.id.showLogCheck);
+		scrollView = (ScrollView)findViewById(R.id.scrollView1);
 		showLogCheck.setChecked(true);
 		IntentFilter mTime = new IntentFilter(Intent.ACTION_TIME_TICK);
 		MyBroadcastReceiver receiver = new MyBroadcastReceiver();
